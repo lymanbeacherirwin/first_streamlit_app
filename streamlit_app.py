2fe9e8125652f59dc7503a5105f40364d4312bab
lymanbeacherirwin <125494187+lymanbeacherirwin@users.noreply.github.com>
2023-02-15 13:12:33 -0600

    Update streamlit_app.py
    Let's Use Another Python Library in our Application

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.text('🍌 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍌 Kale, Spinach & Rocket Smoothie')
 streamlit.header('🍌 Build Your Own Fruit Smoothie')
 streamlit.text('🍌 Hard-Boiled Free-Range Egg')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
